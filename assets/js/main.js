081d9f359d29387940eff11e45bdcc42ef4ce2e5
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2021-11-02 22:44:19 +0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,23 @@ let opennav = document.querySelector('.opennav');
 let mobilMenu = document.querySelector('#mobil-menu');
 let body = document.querySelector('#body');
 
-opennav.addEventListener('click', () => {
-
-if(mobilMenu){
-  mobilMenu.classList.toggle('active')
-}
+opennav.addEventListener('click',() => {
+  mobilMenu.classList.add('active')
 })
+
+let navLink = document.querySelectorAll('.navLink');
+
+navLink.forEach((n) => n.addEventListener("click", () => {
+  mobilMenu.classList.remove('active')
+}));
+
+window.addEventListener('scroll', () => {
+  let header = document.querySelector('#header');
+  if(window.scrollY < 300){
+    header.classList.remove('active')
+  }else{
+    header.classList.add('active')
+  }
+})
+
 
